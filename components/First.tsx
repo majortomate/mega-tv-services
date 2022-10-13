c7def2cca81cb0e850829eb385fd11a8748a145b
Carlos <mac@Majortomate.local>
2022-10-12 19:42:27 -0500

    feat: add first 2 steps to form

---
+++ components/First.tsx
@@ -0,0 +1,13 @@
+function First () {
+  return (
+    <>
+      <div className="fixed top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 text-center">
+        <h1 className="font-bold text-5xl text-center text-black">The Best TV Services around!</h1>
+        <p className="text-xl text-center mt-10">Are you ready to enjoy ultra-best service?</p>
+        <button type="button" className="text-white bg-gradient-to-r from-cyan-500 to-blue-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-cyan-300 dark:focus:ring-cyan-800 font-medium rounded-lg text-lg px-6 py-3.5 text-center mr-2 mb-2 mt-10">LET'S DO IT!</button>
+      </div>
+    </>
+  )
+}
+
+export default First
